d56a6210531feaa7f29e1035e7ce971c3fa970de
Bryce S <bryceness@hotmail.com>
2017-02-08 17:05:39 -0700

    ajax works, now do the jquery stuff

--- sentiment.js
+++ sentiment.js
@@ -8,26 +8,33 @@ $("#getEmotion").click(function(e){
 	var value = $("#emotionalText").val();
 	$("#displayCity").val(value);
 	console.log(value);
-	var myurl="https://gateway.watsonplatform.net/tone-analyzer/api/v3";
+	var myurl="https://watson-api-explorer.mybluemix.net/tone-analyzer/api/v3/tone?version=2016-05-19&text=";
 	myurl += value;
-	myurl += ".json";
 	console.log(myurl);
 	$.ajax(
 	{
 		url : myurl,
+  		username: "6f1e4006-7953-405c-8b0f-dc1ebc5bca40",
+  		password: "NqXrFz2vj5ZF",
 		dataType : "json",
 		success : function(data)
 		{
+			alert("it's working!");
 			console.log(data);
-			var location = data['location']['city'];
-			var temp_string = data['current_observation']['temperature_string'];
-			var current_weather = data['current_observation']['weather'];
-			var everything = "<ul>";
-			everything += "<li>Location: "+location;
-			everything += "<li>Temperature: "+temp_string;
-			everything += "<li>Weather: "+current_weather;
-			everything += "</ul>";
-			$("#weather").html(everything);
+			//the values for each parameter are 0 to 1
+			var emotions = data['document_tone']['tone_categories'][0]
+			var anger = emotions[0]['score'];
+			var disgust = emotions[1]['score'];
+			var fear = emotions[2]['score'];
+			var joy = emotions[3]['score'];
+			var	sadness = emotions[4]['score'];
+			
+			var languageTones = data['document_tone']['tone_categories'][1];
+			//there is: analytical, confident, tentativ
+			var socialTones = data['document_tone']['tone_categories'][2];
+			//there is: openness, conscienciousness, extraversion, agreeable, emotional range,
+			//you can also pick apart each sentence of the input, just get size of "sentence_tone" array first.
+			$("#result").html(everything);
 		}
 	 });
 });
